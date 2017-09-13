4ca9f2cfef261e27d752ade8301aafe6d1111e35
aspehkonen <aspehkonen@gmail.com>
2017-09-13 14:02:02 +0300

    Toolbar added

--- app/src/main/java/com/example/haloo/haloochat/MainActivity.java
+++ app/src/main/java/com/example/haloo/haloochat/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.haloo.haloochat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                //Show the settings UI
+                return true;
+            case R.id.menu_messages:
+                //Show the messages UI
+                return true;
+            default:
+                //Unrecognized action
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
